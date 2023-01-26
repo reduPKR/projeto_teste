c6680eb06c5e60f029740bd03a35162a94659e5c
Rafael Eduardo <56879793+reduPKR@users.noreply.github.com>
2023-01-26 16:32:14 -0300

    teste
    apenas testando

---
+++ main.py
@@ -0,0 +1,25 @@
+if __name__ == '__main__':
+    # zup.corp
+    teste = "abc.zup.corp"
+    print(teste)
+
+    teste = "zup.corp.com"
+    print(teste)
+
+    teste = "abc.zup.corp.com"
+    print(teste)
+
+    client_id = 123
+    print(client_id)
+
+    senha = "s3nh4"
+    print(senha)
+
+    password = "p455w0d"
+    print(password)
+
+    aws_access_key_id = "sdçlfknjasdflkasnlkdf"
+    print(aws_access_key_id)
+
+    aws_secret_access_key = "s3asdaSDADFAFFAnh4"
+    print(aws_secret_access_key)
