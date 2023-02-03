bf71c593c702c0ab8e284efdf92f4c76e17652f0
Rafael Eduardo <56879793+reduPKR@users.noreply.github.com>
2023-02-03 16:56:38 -0300

    novo teste

--- main.py
+++ main.py
@@ -1,3 +1,54 @@
+import br.com.zup.orangetalents.vacina.dtos.UsuarioDto
+import br.com.zup.orangetalents.vacina.mappers.toDto
+import br.com.zup.orangetalents.vacina.mappers.toResponse
+import br.com.zup.orangetalents.vacina.message.request.UsuarioPostRequest
+import br.com.zup.orangetalents.vacina.message.response.UsuarioPostResponse
+import br.com.zup.orangetalents.vacina.service.IServiceGenerics
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+
+br/com/zup/FreteRequest.java
+br.com.zup.FreteRequest
+br.com.zup.FreteRequest$1
+br.com.zup.FreteRequest$Builder
+br/com/zup/CalculaFreteRequest.java
+br.com.zup.CalculaFreteRequest
+br.com.zup.CalculaFreteRequest$1
+br.com.zup.CalculaFreteRequest$Builder
+br/com/zup/FreteRequestOrBuilder.java
+br.com.zup.FreteRequestOrBuilder
+br/com/zup/CalculaFreteResponseOrBuilder.java
+br.com.zup.CalculaFreteResponseOrBuilder
+br/com/zup/FreteReplyOrBuilder.java
+br.com.zup.FreteReplyOrBuilder
+br/com/zup/CalculaFreteResponse.java
+br.com.zup.CalculaFreteResponse
+br.com.zup.CalculaFreteResponse$1
+br.com.zup.CalculaFreteResponse$Builder
+br/com/zup/FreteReply.java
+br.com.zup.FreteReply
+br.com.zup.FreteReply$1
+br.com.zup.FreteReply$Builder
+br/com/zup/Frete.java
+br.com.zup.Frete
+br/com/zup/CalculaFreteRequestOrBuilder.java
+br.com.zup.CalculaFreteRequestOrBuilder
+br/com/zup/FreteServiceGrpc.java
+br.com.zup.FreteServiceGrpc
+br.com.zup.FreteServiceGrpc$1
+br.com.zup.FreteServiceGrpc$2
+br.com.zup.FreteServiceGrpc$3
+br.com.zup.FreteServiceGrpc$FreteServiceBaseDescriptorSupplier
+br.com.zup.FreteServiceGrpc$FreteServiceBlockingStub
+br.com.zup.FreteServiceGrpc$FreteServiceFileDescriptorSupplier
+br.com.zup.FreteServiceGrpc$FreteServiceFutureStub
+br.com.zup.FreteServiceGrpc$FreteServiceImplBase
+br.com.zup.FreteServiceGrpc$FreteServiceMethodDescriptorSupplier
+br.com.zup.FreteServiceGrpc$FreteServiceStub
+br.com.zup.FreteServiceGrpc$MethodHandlers
+
+
 if __name__ == '__main__':
     # zup.corp
     teste = "abc.zup.corp"
